e5440f8ca411ae6ccaff3aa7a527152c6168c02e
Adetops <heritagediwamu@gmail.com>
2023-09-24 09:59:14 -0700

    Configuration Management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This Puppet creates a new file `school` in the `/tmp` directory
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
