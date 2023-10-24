3d35312cb6eca5e8909706cdd4604055354fdca8
Adetops <heritagediwamu@gmail.com>
2023-10-23 17:34:51 -0700

    configure by puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,26 +1,21 @@
 # configures an ubuntu server using puppet as follows:
-# apt-get update
-# apt-get install nginx
-# set X-Served-By -> $HOSTNAME
-# service restart
-
-# updating the ubuntu server
+#	- apt-get update
+#	- apt-get install nginx
+#	- set X-Served-By -> $HOSTNAME
+#	- service restart
 exec { '/usr/bin/env apt-get -y update' : }
 -> package { 'nginx' :
   ensure => installed,
 }
-# add a nginx response header (X-Served-By: hostname)
 -> file { '/var/www/html/index.html' :
   content => 'Holberton School!',
 }
 -> file_line { 'add header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
   after  => 'server_name _;',
-  line   => '\tadd_header X-Served-By ${hostname};',
 }
-->
-# start service
-service { 'nginx':
-  ensure  => 'running',
+-> service { 'nginx':
+  ensure => running,
 }
