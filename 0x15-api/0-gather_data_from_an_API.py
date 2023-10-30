b1fefd38deeab7ad1b4ab0c33fe73ef7d5c2e84e
Adetops <heritagediwamu@gmail.com>
2023-10-30 07:41:18 -0700

    Python API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Returns information about an employee's' TODO list progress.
+        Using requests module
+        First line:
+            Employee EMPLOYEE_NAME is done with tasks
+                (NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+        Second and N next lines display the title of completed
+        tasks: TASK_TITLE
+        (with 1 tabulation and 1 space before the TASK_TITLE)
+"""
+import requests
+import sys
+
+
+# if __name__ == '__main__':
+# url = "https://jsonplaceholder.typicode.com/"
+# empId = int(sys.argv[1])
+
+# employee's endpoint from the url
+# emp_EP = "{}/users/{}".format(url, empId)
+
+# Getting emp's name from endpoint
+# name = requests.get(emp_EP).json().get("name")
+
+# Getting the task endpoint
+# task_EP = "{}/todos".format(url)
+
+# Getting the json of all tasks
+# tasks = requests.get(task_EP).json()
+
+# Getting all tasks for an employee
+# emp_task = [task for task in tasks if task.get("userId") == empId]
+
+# Getting @ll completed tasks
+# taskCmplted = [task for task in emp_task if task.get("completed")]
+
+# print("Employee {} is done with tasks({}/{}):"
+# .format(name.get("name"), len(taskCmplted), len(emp_task)))
+
+# for task in taskCmplted:
+# print("\t {}".format(task.get("title")))
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    name = requests.get("{}users/{}".format(url, sys.argv[1])).json()
+    tasks = requests.get("{}users/{}/todos".format(url, sys.argv[1])).json()
+    c_tasks = [task for task in tasks if task.get("completed") is True]
+    titles = [task.get("title") for task in tasks
+              if task.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name.get("name"), len(c_tasks), len(tasks)))
+    for title in titles:
+        print("\t {}".format(title))
